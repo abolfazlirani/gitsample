87313d3d904100dfbf07e1547b22f95dabe59ab3
abiha94 <abiha9480@gmail.com>
2024-02-11 11:20:18 +0330

    version: 20.3
    Release note:
    1-fixed bug3

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demodss',
+      title: 'Flutter D emodss',
       theme: ThemeData(
         // This is the theme of your application.
         //
